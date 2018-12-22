0af287a291a8dbf3a903bf49ce905ab1cdf0003b
Joel James <me@joelsays.com>
2018-12-22 16:02:46 +0530

    👌 Improve scripts

--- public/js/llc_click.js
+++ public/js/llc_click.js
@@ -1,67 +1,67 @@
-(function ($) {
-    'use strict';
+(function ( $ ) {
+	'use strict';
 
-    /**
-     * All of the code for our admin-specific JavaScript source
-     * should reside in this file.
-     *
-     * Note that this assume you're going to use jQuery, so it prepares
-     * the $ function reference to be used within the scope of this
-     * function.
-     *
-     * From here, we are able to define handlers for when the DOM is
-     * ready:
-     *
-     * $(function() {
+	/**
+	 * All of the code for our admin-specific JavaScript source
+	 * should reside in this file.
+	 *
+	 * Note that this assume you're going to use jQuery, so it prepares
+	 * the $ function reference to be used within the scope of this
+	 * function.
+	 *
+	 * From here, we are able to define handlers for when the DOM is
+	 * ready:
+	 *
+	 * $(function() {
      *
      * });
-     *
-     * Or when the window is loaded:
-     *
-     * $( window ).load(function() {
+	 *
+	 * Or when the window is loaded:
+	 *
+	 * $( window ).load(function() {
      *
      * });
-     *
-     * ...and so on.
-     */
-    $(function () {
+	 *
+	 * ...and so on.
+	 */
+	$( function () {
 
-        // Load comments data on button click.
-        $("#llc_comments_button").on("click", function () {
+		// Load comments data on button click.
+		$( "#llc_comments_button" ).on( "click", function () {
 
-            // Hide button after clicking on it.
-            $(this).hide();
-            // Show loader div. Loader element will be loaded only if enabled.
-            $("#llc-comments-loader").show();
-            // Set data to send through ajax.
-            var data = {
-                action: "llc_load_comments",
-                post: $("#llc_post_id").val()
-            };
-            // Ajax request link.
-            var llcajaxurl = $("#llc_ajax_url").val();
-            // Full url to get comments (Adding parameters).
-            var commentUrl = llcajaxurl + '?' + $.param(data);
-            // Perform ajax request.
-            $.get(commentUrl, function (response) {
-                if (response !== "") {
-                    $("#llc_comments").html(response);
-                    // Get the comment li id from url if exist.
-                    var commentId = document.URL.substr(document.URL.indexOf("#comment"));
-                    // If comment id found, scroll to that comment.
-                    if (commentId.indexOf('#comment') > -1) {
-                        $(window).scrollTop($(commentId).offset().top);
-                    }
-                } else {
-                    // Incase ajax request failed, append an error message.
-                    $("#llc_comments").html('<p style="color: #ff0000;">' + llcstrings.loading_error + '</p>');
-                }
-            });
-        });
+			// Hide button after clicking on it.
+			$( this ).hide();
+			// Show loader div. Loader element will be loaded only if enabled.
+			$( "#llc-comments-loader" ).show();
+			// Set data to send through ajax.
+			var data = {
+				action: "llc_load_comments",
+				post: $( "#llc_post_id" ).val()
+			};
+			// Ajax request link.
+			var llcajaxurl = $( "#llc_ajax_url" ).val();
+			// Full url to get comments (Adding parameters).
+			var commentUrl = llcajaxurl + '?' + $.param( data );
+			// Perform ajax request.
+			$.get( commentUrl, function ( response ) {
+				if ( response !== "" ) {
+					$( "#llc_comments" ).html( response );
+					// Get the comment li id from url if exist.
+					var commentId = document.URL.substr( document.URL.indexOf( "#comment" ) );
+					// If comment id found, scroll to that comment.
+					if ( commentId.indexOf( '#comment' ) > -1 ) {
+						$( window ).scrollTop( $( commentId ).offset().top );
+					}
+				} else {
+					// Incase ajax request failed, append an error message.
+					$( "#llc_comments" ).html( '<p style="color: #ff0000;">' + llcstrings.loading_error + '</p>' );
+				}
+			} );
+		} );
 
-        // Load comments if #comment found in url.
-        if (window.location.href.indexOf("#comment") > -1) {
-            $("#llc_comments_button").trigger("click");
-        }
-    });
-})(jQuery);
+		// Load comments if #comment found in url.
+		if ( window.location.href.indexOf( "#comment" ) > -1 ) {
+			$( "#llc_comments_button" ).trigger( "click" );
+		}
+	} );
+})( jQuery );
--- public/js/llc_scroll.js
+++ public/js/llc_scroll.js
@@ -1,76 +1,76 @@
-(function ($) {
-    'use strict';
+(function ( $ ) {
+	'use strict';
 
-    /**
-     * All of the code for our admin-specific JavaScript source
-     * should reside in this file.
-     *
-     * Note that this assume you're going to use jQuery, so it prepares
-     * the $ function reference to be used within the scope of this
-     * function.
-     *
-     * From here, we are able to define handlers for when the DOM is
-     * ready:
-     *
-     * $(function() {
+	/**
+	 * All of the code for our admin-specific JavaScript source
+	 * should reside in this file.
+	 *
+	 * Note that this assume you're going to use jQuery, so it prepares
+	 * the $ function reference to be used within the scope of this
+	 * function.
+	 *
+	 * From here, we are able to define handlers for when the DOM is
+	 * ready:
+	 *
+	 * $(function() {
      *
      * });
-     *
-     * Or when the window is loaded:
-     *
-     * $( window ).load(function() {
+	 *
+	 * Or when the window is loaded:
+	 *
+	 * $( window ).load(function() {
      *
      * });
-     *
-     * ...and so on.
-     */
-    $(function () {
+	 *
+	 * ...and so on.
+	 */
+	$( function () {
 
-        // Flag to check if comments are loaded already.
-        var llcLoaded = 0;
-        // Function that makes ajax request and loaded comments.
-        var loadComments = function () {
-            // Show loader div and element if not disabled.
-            $("#llc-comments-loader").show();
-            // Data to send over ajax request.
-            var data = {
-                "action": "llc_load_comments",
-                "post": $("#llc_post_id").val(),
-            };
-            // Ajax request link.
-            var llcajaxurl = $("#llc_ajax_url").val();
-            // Full url to get comments (Adding parameters).
-            var commentUrl = llcajaxurl + '?' + $.param(data);
-            // Make ajax request to get comments.
-            $.get(commentUrl, function (response) {
-                if (response !== "") {
-                    $("#llc_comments").html(response);
-                    // Get the comment li id from url if exist.
-                    var commentId = document.URL.substr(document.URL.indexOf("#comment"));
-                    // If comment id found, scroll to that comment.
-                    if (commentId.indexOf('#comment') > -1) {
-                        $(window).scrollTop($(commentId).offset().top);
-                    }
-                }
-            });
-            // Set comments load flag as 1.
-            llcLoaded = 1;
-        }
+		// Flag to check if comments are loaded already.
+		var llcLoaded = 0;
+		// Function that makes ajax request and loaded comments.
+		var loadComments = function () {
+			// Show loader div and element if not disabled.
+			$( "#llc-comments-loader" ).show();
+			// Data to send over ajax request.
+			var data = {
+				"action": "llc_load_comments",
+				"post": $( "#llc_post_id" ).val(),
+			};
+			// Ajax request link.
+			var llcajaxurl = $( "#llc_ajax_url" ).val();
+			// Full url to get comments (Adding parameters).
+			var commentUrl = llcajaxurl + '?' + $.param( data );
+			// Make ajax request to get comments.
+			$.get( commentUrl, function ( response ) {
+				if ( response !== "" ) {
+					$( "#llc_comments" ).html( response );
+					// Get the comment li id from url if exist.
+					var commentId = document.URL.substr( document.URL.indexOf( "#comment" ) );
+					// If comment id found, scroll to that comment.
+					if ( commentId.indexOf( '#comment' ) > -1 ) {
+						$( window ).scrollTop( $( commentId ).offset().top );
+					}
+				}
+			} );
+			// Set comments load flag as 1.
+			llcLoaded = 1;
+		}
 
-        // Load comments if #comment found in url or if no scroll bar available.
-        if (window.location.href.indexOf("#comment") > -1 || document.body.scrollHeight <= window.innerHeight) {
-            loadComments();
-        }
+		// Load comments if #comment found in url or if no scroll bar available.
+		if ( window.location.href.indexOf( "#comment" ) > -1 || document.body.scrollHeight <= window.innerHeight ) {
+			loadComments();
+		}
 
-        // Load comments data on scroll down.
-        $(window).scroll(function () {
-            // Get comments div element.
-            var rect = document.getElementById("llc_comments").getBoundingClientRect();
-            // If comments div is visible, get comments template using ajax.
-            if (rect.top < window.innerHeight && llcLoaded == 0) {
-                // Show loader div and element if not disabled.
-                loadComments();
-            }
-        });
-    });
-})(jQuery);
+		// Load comments data on scroll down.
+		$( window ).scroll( function () {
+			// Get comments div element.
+			var rect = document.getElementById( "llc_comments" ).getBoundingClientRect();
+			// If comments div is visible, get comments template using ajax.
+			if ( rect.top < window.innerHeight && llcLoaded == 0 ) {
+				// Show loader div and element if not disabled.
+				loadComments();
+			}
+		} );
+	} );
+})( jQuery );
